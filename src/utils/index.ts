6f14f1d6a1f7c742265b57e38d7a77b188b3d256
sabin411 <sherasabin411@gmail.com>
2025-01-10 23:00:35 +0545

    chore: 🚀 add CI workflow, update .gitignore, and configure Prettier

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export * from "./args.util";
+export * from "./capitalize.util";
+export * from "./getHookType";
+export * from "./help.util";
+export * from "./kebab-case";
+export * from "./logger.util";
+export * from "./validation.util";
